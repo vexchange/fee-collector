b13497c17b2e338538a6e34fa2741d8d3378bed9
Oliver Chalk <oliverchalk245@gmail.com>
2022-01-28 22:50:00 -0500

    chore(config): add updated fee collector

--- scripts/config.js
+++ scripts/config.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 
 export const PRIVATE_KEY = process.env.PRIVATE_KEY;
-export const FEECOLLECTOR_ADDRESS = "0x5837dbaae7a739fbc264737920512efdf00af1ef";
+export const FEECOLLECTOR_ADDRESS = "0x17d252083c79db33866295078ed955b04e1c61c8";
 export const WVET_ADDRESS = "0xD8CCDD85abDbF68DFEc95f06c973e87B1b5A9997"
 export const MAINNET_NODE_URL = "https://mainnet-node.vexchange.io";

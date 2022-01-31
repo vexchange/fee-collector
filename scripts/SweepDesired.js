98e19fd3093085f294e0aeaa97a0070ef8457bd7
A.L <xenide@users.noreply.github.com>
2022-01-31 09:59:23 +0800

    feature: check for non-zero token balance for sweeping desired token

--- scripts/SweepDesired.js
+++ scripts/SweepDesired.js
@@ -2,6 +2,22 @@ import { Framework } from "@vechain/connex-framework";
 import { Driver, SimpleNet, SimpleWallet } from "@vechain/connex-driver";
 import { PRIVATE_KEY, FEE_COLLECTOR_ADDRESS, MAINNET_NODE_URL } from "./config.js";
 import { isAddress } from "ethers/lib/utils.js";
+import { GetERC20Balance } from "./utils.js";
+
+const MDESIRED_TOKEN_ABI =
+{
+    "inputs": [],
+    "name": "mDesiredToken",
+    "outputs": [
+        {
+            "internalType": "contract IERC20",
+            "name": "",
+            "type": "address"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+}
 
 const SWEEP_DESIRED_ABI =
 {
@@ -29,6 +45,7 @@ const SWEEP_DESIRED_MANUAL_ABI =
 
 async function SweepDesired(aTokenAddress=undefined)
 {
+    console.log(aTokenAddress);
     const lWallet = new SimpleWallet();
     lWallet.import(PRIVATE_KEY);
     const lNet = new SimpleNet(MAINNET_NODE_URL);
@@ -38,7 +55,18 @@ async function SweepDesired(aTokenAddress=undefined)
     const lFeeCollectorContract = lProvider.thor.account(FEE_COLLECTOR_ADDRESS);
 
     const lMethod = aTokenAddress ? lFeeCollectorContract.method(SWEEP_DESIRED_MANUAL_ABI)
-                                 : lFeeCollectorContract.method(SWEEP_DESIRED_ABI);
+                                  : lFeeCollectorContract.method(SWEEP_DESIRED_ABI);
+
+    const lDefaultDesiredToken = (await lFeeCollectorContract.method(MDESIRED_TOKEN_ABI).call()).decoded['0'];
+    const lTokenBalance = await GetERC20Balance(aTokenAddress ? aTokenAddress : lDefaultDesiredToken,
+                                FEE_COLLECTOR_ADDRESS,
+                                lProvider);
+
+    if (lTokenBalance.eq(0))
+    {
+        console.log("Balance for", aTokenAddress ? aTokenAddress : lDefaultDesiredToken, "is zero. Not sweeping this token");
+        return;
+    }
 
     try
     {

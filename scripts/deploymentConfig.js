60b1e864995a911c6e1a5502ab3c65ced3d6e8d1
Anderson Aguiar <revsoft10@gmail.com>
2022-07-07 16:47:10 +1000

    chore(cleanup): remove unused address

--- scripts/deploymentConfig.js
+++ scripts/deploymentConfig.js
@@ -21,7 +21,6 @@ export default {
     wvetAddress: "0xD8CCDD85abDbF68DFEc95f06c973e87B1b5A9997",
     timelockAddress: "0x41D293Ee2924FF67Bd934fC092Be408162448f86",
     governorAlphaAddress: "0xa0a636893Ed688076286174Bc23b34C31BED3089",
-    feeCollectorAddress: "0x17D252083c79Db33866295078ED955B04e1C61c8",
     feeCollectorVexAddress:  "0x10445a86645838306194c07f81ebd00bb7b82598",
     feeCollectorWvetAddress:  "0xc2ccf0af1b34367b639d0fd7bb4335da12bcc798",
     distributorAddress: "0x72ee1c849b7353ad1452e56af136e4b0ff68a07e",

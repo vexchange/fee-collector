c2340c98c1a334e9bc42ae9ed12183b7c90d145d
Anderson Aguiar <revsoft10@gmail.com>
2022-07-07 14:58:16 +1000

    chore(refactor): use connex

--- scripts/deployDistributor.js
+++ scripts/deployDistributor.js
@@ -1,15 +1,19 @@
-// ES5 style
-const config = require("./deploymentConfig");
-const thorify = require("thorify").thorify;
-const Web3 = require("web3");
-const DistributorContract = require(config.pathToDistributorJson);
-const readlineSync = require("readline-sync");
+import { Framework } from "@vechain/connex-framework";
+import { Driver, SimpleNet, SimpleWallet } from "@vechain/connex-driver";
+import { PRIVATE_KEY, DISTRIBUTOR_ADDRESS } from "./config.js";
+import { abi } from "thor-devkit";
+import config from "./deploymentConfig.js";
+import fs from "fs";
+import path from "path";
+const __dirname = path.resolve();
+
+const DistributorContract = JSON.parse(
+  fs.readFileSync(path.join(__dirname, config.pathToDistributorJson), "utf-8")
+);
 
 let network = null;
-if (process.argv.length < 3) {
-  console.error(
-    "Usage: node scripts/deployDistributor [mainnet|testnet]"
-  );
+if (process.argv.l0ength < 3) {
+  console.error("Usage: node scripts/deployDistributor [mainnet|testnet]");
   process.exit(1);
 }
 
@@ -19,23 +23,24 @@ if (network === undefined) {
   process.exit(1);
 }
 
-const web3 = thorify(new Web3(), network.rpcUrl);
-web3.eth.accounts.wallet.add(config.privateKey);
+// const web3 = thorify(new Web3(), network.rpcUrl);
+// web3.eth.accounts.wallet.add(config.privateKey);
 
-deployDistributor = async () => {
-  // This is the address associated with the private key
-  const walletAddress = web3.eth.accounts.wallet[0].address;
+const DISTRIBUTOR_CONSTRUCTOR = {
+  inputs: [
+    {
+      internalType: "contract IERC20",
+      name: "aIncomingToken",
+      type: "address",
+    },
+  ],
+  stateMutability: "nonpayable",
+  type: "constructor",
+};
 
-  console.log("Using wallet address:", walletAddress);
-  console.log("Using RPC:", web3.eth.currentProvider.RESTHost);
-
+(async () => {
   try {
-    let transactionReceipt = null;
-
-    console.log(
-      "Attempting to deploy contract:",
-      config.pathToDistributorJson
-    );
+    console.log("Attempting to deploy contract:", config.pathToDistributorJson);
 
     if (network.name == "mainnet") {
       let input = readlineSync.question(
@@ -44,27 +49,44 @@ deployDistributor = async () => {
       if (input != "y") process.exit(1);
     }
 
-    const distributorContract = new web3.eth.Contract(DistributorContract.abi);
+    let receipt = null;
+    const lWallet = new SimpleWallet();
+    lWallet.import(PRIVATE_KEY);
+    const lNet = new SimpleNet(network.rpcUrl);
+    const lDriver = await Driver.connect(lNet, lWallet);
+    const connex = new Framework(lDriver);
+    const coder = new abi.Function(DISTRIBUTOR_CONSTRUCTOR);
+    const data =
+      DistributorContract.bytecode.object +
+      coder
+        .encode(config.addresses.wvetAddress)
+        .slice(10 /* remove 0x prefix and 4bytes sig */);
 
-    await distributorContract
-      .deploy({
-        data: DistributorContract.bytecode.object,
-        arguments: [config.addresses.wvetAddress],
-      })
-      .send({ from: walletAddress })
-      .on("receipt", (receipt) => {
-        transactionReceipt = receipt;
-      });
+    const { txid } = await connex.vendor
+      .sign("tx", [{ to: null, value: 0, data }])
+      .request();
 
-    console.log({
-      "Transaction Hash": transactionReceipt.transactionHash,
-      "Contract Successfully deployed at address": transactionReceipt.contractAddress
-    })
+    console.info(`Transaction: ${txid}`);
+
+    const transaction = connex.thor.transaction(txid);
+
+    while (!receipt) {
+      await connex.thor.ticker().next();
+      receipt = await transaction.getReceipt();
+    }
+
+    console.log(receipt);
+
+    if (receipt.reverted) {
+      console.log("Transaction was reverted");
+    } else {
+      console.log("Transaction was successful");
+    }
 
     // await renounceMastership(transactionReceipt.contractAddress);
   } catch (error) {
     console.log("Deployment failed with:", error.message);
   }
-};
 
-deployDistributor();
+  process.exit(1);
+})();

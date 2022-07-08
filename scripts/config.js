51e96880c89d3e671cdadd4c78850c6be17e3845
Anderson Aguiar <revsoft10@gmail.com>
2022-07-08 16:39:33 +1000

    chore(config): change config

--- scripts/config.js
+++ scripts/config.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 
 export const PRIVATE_KEY = process.env.PRIVATE_KEY;
+export const V2FACTORY_ADDRESS = "0xB312582C023Cc4938CF0faEA2fd609b46D7509A2";
 export const FEE_COLLECTOR_ADDRESS = "0x17D252083c79Db33866295078ED955B04e1C61c8";
 export const FEE_COLLECTOR_VEX_ADDRESS = "0x10445a86645838306194c07f81ebd00bb7b82598";
 export const FEE_COLLECTOR_WVET_ADDRESS = "0xc2ccf0af1b34367b639d0fd7bb4335da12bcc798";
@@ -10,3 +11,7 @@ export const WVET_ADDRESS = "0xD8CCDD85abDbF68DFEc95f06c973e87B1b5A9997";
 export const VEX_ADDRESS = "0x0BD802635eb9cEB3fCBe60470D2857B86841aab6";
 export const VTHO_ADDRESS = "0x0000000000000000000000000000456e65726779";
 export const MAINNET_NODE_URL = "https://mainnet-node.vexchange.io";
+export const TESTNET_NODE_URL = "http://testnet02.vechain.fi.blockorder.net";
+export const TIMELOCK_ADDRESS = "0x41D293Ee2924FF67Bd934fC092Be408162448f86";
+export const DISTRIBUTOR_JSON_PATH = "/out/Distributor.sol/Distributor.json";
+export const FEE_COLLECTOR_JSON_PATH = "/out/FeeCollector.sol/FeeCollector.json";
